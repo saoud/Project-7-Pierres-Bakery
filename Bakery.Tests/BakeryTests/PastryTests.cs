e366645193aff492b4266de4aa81d290dc2ae3ff
Saoud Rana <saoud@saoud.dev>
2021-04-24 15:04:13 -0700

    fixing file stucture and adding first test

--- Bakery.Tests/BakeryTests/PastryTests.cs
+++ Bakery.Tests/BakeryTests/PastryTests.cs
@@ -1,2 +1,15 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using TDDcsharp;
+using Bakery.Models;
+namespace Bakery.Tests
+{
+  [TestClass]
+  public class PastriesTests
+  {
+  [TestMethod] // How many pastries?
+    public void PastriesChecker_TestShouldTellNumberOfPastries_PastriesHasNumber()
+    {
+      Pastries pastriesTest = new Pastries(30);
+      Assert.AreEqual(30, pastriesTest.AmountOfPastries);
+    }
+  }
+}
