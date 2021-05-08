b3bd48a8f4bf172fc2642f9dde53918133f95bc0
Saoud Rana <saoud@saoud.dev>
2021-05-08 16:25:11 -0700

    2nd test good fail

--- Bakery.Tests/BakeryTests/PastryTests.cs
+++ Bakery.Tests/BakeryTests/PastryTests.cs
@@ -1,9 +1,22 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Bakery.Models;
+
 namespace Bakery.Tests
 {
   [TestClass]
-  public class PastriesTests
+  public class PastryTests
   {
+    [TestMethod]
+    public void PastryConstructor_CreatesInstanceOfPastry_Pastry()
+    {
+      Pastry newPastry = new Pastry(0);
+      Assert.AreEqual(typeof(Pastry), newPastry.GetType());
+    }
+    [TestMethod]
+    public void GetCost_ReturnsCost_int()
+    {
+      Pastry newPastry = new Pastry(0);
+      Assert.AreEqual(1, newPastry.Cost);
+    }
   }
 }
