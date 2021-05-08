2b029d4f8e98e38d8eb2bcd83edddd0d3e14315e
Saoud Rana <saoud@saoud.dev>
2021-05-08 16:39:03 -0700

    takg in in user input for bread and checking to make sure its valid

--- Bakery/program.cs
+++ Bakery/program.cs
@@ -11,6 +11,13 @@ namespace Bakery
       Console.WriteLine("Here is our menu:");
       Console.WriteLine("Bread: $5 per loaf. Buy 2, Get 1 free!");
       Console.WriteLine("Pastry: $2 each. Get 3 for $5!");
+      Console.WriteLine("How many bread loaves would you like to order?");
+      bool breadOrderSuccess = int.TryParse(Console.ReadLine(), out int breadOrder);
+      while((breadOrderSuccess == false) || (breadOrder < 0))
+      {
+        Console.WriteLine("That is not a valid input. Please enter the number of bread loaves you would like. Enter 0 if you do not want any bread loaves.");
+        breadOrderSuccess = int.TryParse(Console.ReadLine(), out breadOrder);
+      }
     }
   }
 }
