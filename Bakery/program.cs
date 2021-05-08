cc1fb9115fbe1bf17aed61aa5f8ff187008892ce
Saoud Rana <saoud@saoud.dev>
2021-05-08 16:37:26 -0700

    writing menu in console

--- Bakery/program.cs
+++ Bakery/program.cs
@@ -1,14 +1,16 @@
 using Bakery.Models;
 using System;
-using System.Collections.Generic;
 
 namespace Bakery
 {
-  class Program
+  public class Program
   {
-static void Main()
+    public static void Main()
     {
-      
+      Console.WriteLine("Welcome to Pierre's Bakery!");
+      Console.WriteLine("Here is our menu:");
+      Console.WriteLine("Bread: $5 per loaf. Buy 2, Get 1 free!");
+      Console.WriteLine("Pastry: $2 each. Get 3 for $5!");
+    }
   }
-}
 }
